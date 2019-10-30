4d8e7d4a00b4678f7562b411d8c74cfeb97815ba
Destiny Malone <destinymalone15@gmail.com>
2019-10-30 16:25:28 -0500

    I was not able to modify the json file

--- concert_tickets/app.py
+++ concert_tickets/app.py
@@ -10,47 +10,82 @@ TICKET_FILE = "./ticket.txt"
 SALES_TAX = 0.07  # 7% Sales Tax
 
 
-def pick_show():
-    with open(SHOWS_FILE, "r") as file:
-        ticket = input("What show: ")
-        for ticket in collection:
-            print(ticket["artist"])
-            for show in collection:
-                print([show])
-        json.load(file)
+def get_menu():
+    with open(SHOWS_FILE) as menu:
+        return json.load(menu)
 
 
-def sell():
-    with open(SHOWS_FILE, "r") as file:
-        json.load(file)
-
-    for show in collection:
-        for show["tickets"] in show:
-            print(show["price"])
+def show_menu(collection):
+    for element in collection:
+        dots = 18 - len(element["artist"])
+        print(f'{element["artist"]}{dots * "."}${element["price"]:.2f}')
 
 
-def transaction():
-    with open(SHOWS_FILE) as file:
-        quantity = int(input("How many: "))
-        for choice in collection:
-            for tickets in choice:
-                if tickets != 0:
-                    ticket -= 1
-                elif choice["tickets"] == "SOLD OUT":
+def make_choice(collection):
+    valid_options = []
+    for item in collection:
+        valid_options.append(item.get("artist"))
+    while True:
+        choice = input("Choice: ")
+        if choice in valid_options:
+            return choice
+
+
+def ticket_update(collection, choice):
+    with open(SHOWS_FILE, "r") as files:
+        json.load(files)
+
+    with open(SHOWS_FILE, "a") as file:
+        ticket = input("How many tickets did you get: ")
+        for item in collection:
+            if item.get("artist") == choice:
+                item["tickets"]
+                if item["tickets"] == "SOLD OUT":
                     print("Sold Out")
-                elif quantity > choice["tickets"]:
-                    print(choice["tickets"])
+                    make_choice(collection)
                 else:
-                    print("Sold out!!")
-        json.load(file)
+                    del item["tickets"]
+                    item.update(tickets - int(ticket))
+
+
+def get_price(collection, choice):
+    for item in collection:
+        if item.get("artist") == choice:
+            return item.get("price")
+
+
+def get_code(collection, choice):
+    for item in collection:
+        if item.get("artist") == choice:
+            return item.get("code")
+
+
+def save_file(choice, price, code):
+    name = input("First and Last Name: ")
+    tickets = int(input("How many tickets did you get: "))
+    with open(TRANSACTIONS_FILE, "a") as file:
+        file.write(
+            f"\n{name}, {choice}, {code}, {tickets}, {'$'}{price * tickets}, {'$'}{(SALES_TAX * tickets)}, {datetime.now()}"
+        )
 
 
 def main():
     print("Welcome to The Jefferson venue ticket purchasing tool!")
-    # this is where the code you write goes
-    pick_show()
-    sell()
-    transaction()
+    data = get_menu()
+    show_menu(data)
+    choice = make_choice(data)
+    tickets = int(input("How many tickets: "))
+    price = get_price(data, choice)
+    code = get_code(data, choice)
+    ticket_amount = ticket_update(data, choice)
+    total = (SALES_TAX + price) * tickets
+    print(
+        "{} tickets for {} coming up! That will be ${:.2f}".format(
+            tickets, choice, total
+        )
+    )
+    save_file(choice, price, code)
+    print("Have a good day.")
 
 
 if __name__ == "__main__":
